74dc59af8bd634d986b333d14b6b6cb82d09016c
root <root@15bbd7aedf94>
2020-12-29 12:49:47 +0200

    Add support of Symfony 5

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,8 +18,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('mp_doctrine');
+        $treeBuilder = new TreeBuilder('mp_doctrine');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // symfony < 4.2 support
+            $rootNode = $treeBuilder->root('mp_doctrine');
+        }
 
         $this->addORMSection($rootNode);
 
