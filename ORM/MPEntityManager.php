39e9f3583e4171843f3a771405ed5e93bdca34c5
Serhii Popov <popow.serhii@gmail.com>
2021-10-25 10:20:03 +0300

    Add MpEntityManagerInterface

--- ORM/MPEntityManager.php
+++ ORM/MPEntityManager.php
@@ -12,37 +12,42 @@ use Mediapark\MPDoctrineRepositoryExtensionBundle\ORM\Repository;
 use Mediapark\MPDoctrineRepositoryExtensionBundle\ORM\Configuration\InterfaceMPConfiguration;
 //use Model\MainBundle\Annotations\RepositoryResolver;
 
-class MPEntityManager extends EntityManager {
-
+class MPEntityManager extends EntityManager implements MpEntityManagerInterface
+{
     private $repositoryFactory;
+
     public static $count = 0;
 
-    protected function __construct(Connection $conn, Configuration $config, EventManager $eventManager) {
+    protected function __construct(Connection $conn, Configuration $config, EventManager $eventManager)
+    {
         parent::__construct($conn, $config, $eventManager);
-
         $this->setRepositoryFactory($config->getRepositoryFactory());
     }
 
-    public static function customCreate(InterfaceMPConfiguration $MPConfiguration, $conn, Configuration $config, EventManager $eventManager = null) {
+    public static function customCreate(
+        InterfaceMPConfiguration $MPConfiguration,
+        $conn,
+        Configuration $config,
+        EventManager $eventManager = null
+    ) {
         if (!$config->getMetadataDriverImpl()) {
             throw ORMException::missingMappingDriverImpl();
         }
-
         $config->setRepositoryFactory($MPConfiguration->getRepositoryFactory());
-
+        
         switch (true) {
             case (is_array($conn)):
                 $conn = \Doctrine\DBAL\DriverManager::getConnection(
-                    $conn, $config, ($eventManager ?: new EventManager())
+                    $conn,
+                    $config,
+                    ($eventManager ?: new EventManager())
                 );
                 break;
-
             case ($conn instanceof Connection):
                 if ($eventManager !== null && $conn->getEventManager() !== $eventManager) {
-                     throw ORMException::mismatchedEventManager();
+                    throw ORMException::mismatchedEventManager();
                 }
                 break;
-
             default:
                 throw new \InvalidArgumentException("Invalid argument: " . $conn);
         }
@@ -50,49 +55,25 @@ class MPEntityManager extends EntityManager {
         return new MPEntityManager($conn, $config, $conn->getEventManager());
     }
 
-    public function getRepository($entityName, $repositoryBundle = null) {
+    public function getRepository($entityName, $repositoryBundle = null)
+    {
         return $this->getRepositoryFactory()->getRepository($this, $entityName, $repositoryBundle);
     }
 
-    protected function setRepositoryFactory(RepositoryFactory $repositoryFactory) {
+    protected function setRepositoryFactory(RepositoryFactory $repositoryFactory)
+    {
         $this->repositoryFactory = $repositoryFactory;
     }
 
-    protected function getRepositoryFactory() {
+    protected function getRepositoryFactory()
+    {
         return $this->repositoryFactory;
     }
 
-    public function get($entityName) {
-        /*if (strpos($className, ':') !== false) {
-            list($namespaceAlias, $simpleClassName) = explode(':', $className);
-            var_dump($namespaceAlias);
-            var_dump($simpleClassName);
-            die()
-            //$realClassName = $this->getFqcnFromAlias($namespaceAlias, $simpleClassName);
-        }*/
-
-        $this->entityResolver->getRepositoryByEntity($entityName);
-        die();
-
-        var_dump("SHIT");
-        var_dump($entityName);
-        die();
-        $repositoryClass = $this->resolveByEntity($entityName);
-
-        $this->entityResolver = new RepositoryResolver($repositoryClass);
-        var_dump($this->entityResolver);
-        die();
-
-        $er = $this->container->get($entity_name);
-        $this->metaData = $this->getClassMetadata($er->getEntityName());        
-        $er->init($this->em, $this->metaData);
-        return $er;
-    }
-
-    private function resolveByEntity($entityName) {
+    private function resolveByEntity($entityName)
+    {
         $entityName = ltrim($entityName, '\\');
         var_dump($entityName);
         die();
     }
-
 }
---
+++ ORM/MpEntityManagerInterface.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2021 Mediapark
+ * This source file is subject to The MIT License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/MIT
+ *
+ * @category Mediapark
+ * @author Serhii Popov <serhii.popov@mediapark.com>
+ * @license https://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace Mediapark\MPDoctrineRepositoryExtensionBundle\ORM;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+interface MpEntityManagerInterface extends EntityManagerInterface
+{
+}
